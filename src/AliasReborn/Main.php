4273d8db7075c8dc977f8a46a3d9046996c5cda8
iZeaoGamer <jameshayward14@icloud.com>
2020-05-24 16:26:23 +0100

    Update Main.php

--- src/AliasReborn/Main.php
+++ src/AliasReborn/Main.php
@@ -11,6 +11,7 @@ use pocketmine\Player;
 use pocketmine\level\Level;
 use pocketmine\Server;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerLoginEvent;
 	
 	class Main extends PluginBase implements Listener{
 	
@@ -20,9 +21,31 @@ use pocketmine\event\player\PlayerJoinEvent;
 			if(is_dir($this->getDataFolder() . "alias/") == false){
 				@mkdir($this->getDataFolder() . "alias/", 0777, true);
 				}
-			$this->getLogger()->info("Version: 1.0.2 for API: 3.0.0");
+			$this->getLogger()->info("Version: 2.0.0 for API: 3.0.0");
+			$this->seedevice = $this->getServer()->getPluginManager()->getPlugin("SeeDevice");
 		}
-		
+		public function onLoad(PlayerLoginEvent $event){
+					$p = $e->getPlayer();
+			$name = $p->getName();
+			$ip = $p->getAddress();
+			$uid = $p->getUniqueId();
+			$cid = $p->getClientId();
+			$xid = $p->getXuid();
+			//$track = $this->getConfig()->get("track");
+	//  if(strtolower($track) === "ip"){
+				if(file_exists($this->getDataFolder() . "alias/" . $ip)){ //|| file_exists($this->getDataFolder() . "alias/". $modal){
+					$file = explode(",\n", file_get_contents($this->getDataFolder() . "alias/" . $ip, true));
+					if(!in_array($name, $file)){
+						file_put_contents($this->getDataFolder() . "alias/" . $ip, $name . ",\n", FILE_APPEND);
+						
+						$this->getLogger()->info("§aThis seems to be an alt on the same IP Address.\nyou should look into it if this person has been banned before or not.");
+					}
+					} else {
+						file_put_contents($this->getDataFolder() . "alias/" . $ip, $name . ",\n");
+						$this->getLogger()->info("§5No records found for this person.");
+					}
+		  $this->getLogger()->info("Checked IP Addresses.");
+		}
 		public function onJoin(PlayerJoinEvent $e){
 			$p = $e->getPlayer();
 			$name = $p->getName();
@@ -30,99 +53,53 @@ use pocketmine\event\player\PlayerJoinEvent;
 			$uid = $p->getUniqueId();
 			$cid = $p->getClientId();
 			$xid = $p->getXuid();
-			$track = $this->getConfig()->get("track");
-			if(strtolower($track) === "ip"){
-				if(file_exists($this->getDataFolder() . "alias/" . $ip)){
-					$file = explode(",\n", file_get_contents($this->getDataFolder() . "alias/" . $ip, true));
+			$modal = $this->seedevice->getUsd($p);
+		//	$track = $this->getConfig()->get("track");
+	//  if(strtolower($track) === "ip"){
+				if(file_exists($this->getDataFolder() . "alias/" . $modal)){ //|| file_exists($this->getDataFolder() . "alias/". $modal){
+					$file = explode(",\n", file_get_contents($this->getDataFolder() . "alias/" . $modal, true));
 					if(!in_array($name, $file)){
-						file_put_contents($this->getDataFolder() . "alias/" . $ip, $name . ",\n", FILE_APPEND);
-						$this->getLogger()->info("§aThis seems to be an alt\nyou should look into it if this person has been banned before or not.");
+						file_put_contents($this->getDataFolder() . "alias/" . $modal, $name . ",\n", FILE_APPEND);
+						$this->getLogger()->info("§aThis seems to be an alt on the same Device.\nyou should look into it if this person has been banned before or not.");
 					}
 					} else {
-						file_put_contents($this->getDataFolder() . "alias/" . $ip, $name . ",\n");
+						file_put_contents($this->getDataFolder() . "alias/" . $modal, $name . ",\n");
 						$this->getLogger()->info("§5No records found for this person.");
 					}
+$this->getLogger()->info("Device Checked.");
 				}
-			if(strtolower($track) === "uid"){
-				if(file_exists($this->getDataFolder() . "alias/" . $uid)){
-					$file = explode(",\n", file_get_contents($this->getDataFolder() . "alias/" . $uid, true));
-					if(!in_array($name, $file)){
-							file_put_contents($this->getDataFolder() . "alias/" . $uid, $name . ",\n", FILE_APPEND);
-						$this->getLogger()->info("§aThis seems to be an alt\nyou should look into it if this person has been banned before or not.");
-					}
-					} else {
-						file_put_contents($this->getDataFolder() . "alias/" . $uid, $name . ",\n");
-						$this->getLogger()->info("§5No records found for this person.");
-					}
-				}
-			if(strtolower($track) === "cid"){
-				if(file_exists($this->getDataFolder() . "alias/" . $cid)){
-					$file = explode(",\n", file_get_contents($this->getDataFolder() . "alias/" . $cid, true));
-					if(!in_array($name, $file)){
-							file_put_contents($this->getDataFolder() . "alias/" . $cid, $name . ",\n", FILE_APPEND);
-						$this->getLogger()->info("§aThis seems to be an alt\nyou should look into it if this person has been banned before or not.");
-					}
-					} else {
-						file_put_contents($this->getDataFolder() . "alias/" . $cid, $name . ",\n");
-						$this->getLogger()->info("§5No records found for this person.");
-					}
-				}
-			if(strtolower($track) === "xid"){
-				if(file_exists($this->getDataFolder() . "alias/" . $xid)){
-					$file = explode(",\n", file_get_contents($this->getDataFolder() . "alias/" . $xid, true));
-					if(!in_array($name, $file)){
-							file_put_contents($this->getDataFolder() . "alias/" . $xid, $name . ",\n", FILE_APPEND);
-						$this->getLogger()->info("§aThis seems to be an alt.\nyou should look into it if this person has been banned before or not.");
-					}
-					} else {
-						file_put_contents($this->getDataFolder() . "alias/" . $xid, $name . ",\n");
-						$this->getLogger()->info("§5No records found for this person.");
-					}
-				}
-		}
-		
+	}
 		public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
 			switch(strtolower($command->getName())){
 				case "alias":
-				if(isset($args[0])){
+				if(!isset($args[1])){
+					$sender->sendMessage(TextFormat::colorize("&5Please use: &6/alias <player> <ip/device>"));
+					return true;
+				}
 					$p = $this->getServer()->getPlayer($args[0]);
-					if($p != null && $p->isOnline() && $p instanceof Player){
+					if($p == null && !$p->isOnline() && !$p instanceof Player){
+						$sender->sendMessage(TextFormat::colorize("&cPlayer &4" . $args[0] . " &cnot found."));
+						return true;
+					}
 						$ip = $p->getAddress();
-						$uid = $p->getUniqueId();
-						$cid = $p->getClientId();
-						$xid = $p->getXuid();
-						$track = $this->getConfig()->get("track");
-						if(strtolower($track) === "ip"){
-							$contents = file_get_contents($this->getDataFolder() . "alias/" . $ip, true);
+							switch($args[1]){
+            
+								case "ip":
+$contents = file_get_contents($this->getDataFolder() . "alias/" . $ip, true);
 							$final_list = implode(", ", array_unique(explode(",\n", $contents)));
 							$sender->sendMessage("§aHere are the accounts this player is using on that ip: §b" $final_list);
-							}
-						if(strtolower($track) === "uid"){
-							$contents = file_get_contents($this->getDataFolder() . "alias/" . $uid, true);
+							break;
+								case "device":
+									$modal = $this->seedevice->getUsd($p);
+							$contents = file_get_contents($this->getDataFolder() . "alias/" . $modal, true);
 							$final_list = implode(", ", array_unique(explode(",\n", $contents)));
-							$sender->sendMessage("§aHere are the accounts this player is using on that UID: §b" $final_list);
+							$sender->sendMessage("§5Here are the accounts this player is using on that Device: §6" $final_list);
+							
+					break;
 							}
-						if(strtolower($track) === "cid"){
-							$contents = file_get_contents($this->getDataFolder() . "alias/" . $cid, true);
-							$final_list = implode(", ", array_unique(explode(",\n", $contents)));
-							$sender->sendMessage("§aHere are the accounts this player is using on that CID: §b" $final_list);
-							}
-						if(strtolower($track) === "xid"){
-							$contents = file_get_contents($this->getDataFolder() . "alias/" . $xid, true);
-							$final_list = implode(", ", array_unique(explode(",\n", $contents)));
-							$sender->sendMessage("§aHere are the accounts this player is using on that XID: §b" $final_list);
-							}
-							return true;
-						} else {
-						$sender->sendMessage("§2Player not online or does not exist.");
-						return false;
-						}
-					} else {
-					$sender->sendMessage("§dPlease use: §6/alias <player>");
-					return false;
-					}
-				break;
-			}
+					         return true;
+						
 		}
 		
 	}
+}
